b3c007040b83cd104071eb770afa88c874b36548
José Miguel Moreno <josemmo@pm.me>
2019-08-07 13:39:09 +0200

    Updated deploy
    - Added skip_cleanup
    - Updated deploy script

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -5,21 +5,22 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
 # Pull remote changes
-git status # TODO
+echo "Stashing changes"
 git stash
+echo "Pulling latest commits"
 git checkout master
 git pull origin master
+echo "Restoring stashed changes"
 git stash pop
 
 # Commit latest libVLC files
-git status # TODO
+echo "Committing changes"
 cd ./libvlc
 git add . --all
 cd ..
-git status # TODO
 git commit -m "libVLC v$LIBVLC_VERSION" -m "[ci skip]"
-git status # TODO
 
 # Upload files
+echo "Pushing commits"
 git remote add origin-master https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git > /dev/null 2>&1
 git push --set-upstream origin-master master
